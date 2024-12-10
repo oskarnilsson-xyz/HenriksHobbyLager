6b63dbb170d5a43f9f9cf41fbc0c553b4956d035
oskarnilsson-xyz <114058419+oskarnilsson-xyz@users.noreply.github.com>
2024-12-10 11:29:52 +0100

    Refactor; Initial OOP classes implementation.

---
+++ HenriksHobbyLager/Interfaces/IProductFacade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HenriksHobbyLager.Interfaces
+{
+    public interface IProductFacade
+    {
+        void AddProduct(Product product);
+        void RemoveProduct(Product product);
+        void UpdateProduct(Product product);
+        Product GetProductById(int id);
+        List<Product> GetAllProducts();
+        IEnumerable<Product> SearchProducts(string searchTerm);
+    }
+}
---
+++ HenriksHobbyLager/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HenriksHobbyLager.Interfaces
+
+    public interface IRepository<T>
+{
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(int id);
+    IEnumerable<T> Search(Func<T, bool> predicate);
+}
